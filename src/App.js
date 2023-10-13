83de28a5d3e1df52c47fe6fad1e94073b0b6a08f
Dinesh <dineshsharmanit7@gmail.com>
2023-10-13 13:57:00 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import Header from './component/Header';
+// import { Outlet } from 'react-router-dom';
+// import toast ,{Toaster} from 'react-hot-toast';
+// import { useEffect } from "react";
+// import { setDataProduct } from "./redux/productSlide";
+// import { useDispatch, useSelector } from "react-redux";
+
+// function App() {
+//   const dispatch = useDispatch()
+//   const productData = useSelector((state)=>state.product)
+ 
+//   useEffect(()=>{
+//     (async()=>{
+//       const res = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/product`)
+//       const resData = await res.json()
+//       console.log(resData)
+//       dispatch(setDataProduct(resData))
+//     })()
+//   },[])
+//   console.log(productData)
+//   return (
+//      <>
+//      <Toaster/>
+//      <div >
+//       <Header/>
+//       <main className='pt-16 bg-slate-100 min-h-[calc(100vh)]'>
+//         <Outlet/>
+//       </main>
+//      </div>
+//      </>
+//   );
+// }
+
+// export default App;
+
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setDataProduct } from "./redux/productSlide";
+import { Outlet } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import Header from "./component/Header";
+
+function App() {
+  const dispatch = useDispatch();
+  const productData = useSelector((state) => state.product);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/product`);
+        const resData = await res.json();
+        console.log(resData);
+        dispatch(setDataProduct(resData));
+      } catch (error) {
+        console.error("Error fetching product data:", error);
+      }
+    };
+
+    fetchData();
+  }, [dispatch]);
+
+  console.log(productData);
+
+  return (
+    <>
+      <Toaster />
+      <div>
+        <Header />
+        <main className="pt-16 bg-slate-100 min-h-[calc(100vh)]">
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
